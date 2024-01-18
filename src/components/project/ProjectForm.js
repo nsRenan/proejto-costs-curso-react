1f4ca06341b75f7453c4633ed13ace03edd16276
Renan Nobre <106410660+nsRenan@users.noreply.github.com>
2024-01-18 14:03:54 -0300

    Update ProjectForm.js

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -11,7 +11,7 @@ function ProjectForm({handleSubmit, btnText, projectData}){
     const [project, setProject] = useState(projectData || [])
 
     useEffect(() => {
-        fetch("http://localhost:5000/categories", {
+        fetch("http://localhost:3000/categories", {
         method: "GET",
         headers: {
             'Content-Type': 'application/json'
